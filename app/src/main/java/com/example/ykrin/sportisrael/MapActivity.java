88f2e79a56b03cf8495a58afd2a1ab9bcac8aa8c
Yuval Krinsky <ykrinsky@gmail.com>
2020-02-07 12:18:46 +0200

    Added inital version of goolge map: compiling but not displaying map, and added google api key.

--- app/src/main/java/com/example/ykrin/sportisrael/MapActivity.java
+++ app/src/main/java/com/example/ykrin/sportisrael/MapActivity.java
@@ -3,14 +3,21 @@ package com.example.ykrin.sportisrael;
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapActivity extends AppCompatActivity {
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     BottomNavigationView menu_bar_view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Retrieve the content view that renders the map.
         setContentView(R.layout.activity_map);
 
         menu_bar_view = (BottomNavigationView)findViewById(R.id.navigation_bar);
@@ -18,5 +25,20 @@ public class MapActivity extends AppCompatActivity {
         NavigationBar navigation_bar = new NavigationBar(this);
         // Set actions for pressing menu bar options.
         menu_bar_view.setOnNavigationItemSelectedListener(navigation_bar);
+
+        // Get the SupportMapFragment and request notification
+        // when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        // Add a marker in Sydney, Australia,
+        // and move the map's camera to the same location.
+        LatLng sydney = new LatLng(-33.852, 151.211);
+        googleMap.addMarker(new MarkerOptions().position(sydney)
+                .title("Marker in Sydney"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
